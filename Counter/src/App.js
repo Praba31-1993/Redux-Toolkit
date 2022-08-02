c146e47414970d907cd598af0230607fb9bd3484
Praba31-1993 <prabamymotowd@gmail.com>
2022-08-02 21:45:05 +0530

    counter app

---
+++ Counter/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Login from './Login';
+import './style.css'
+
+function App() {
+  return (
+    <div className="g-signin">
+      <Login/>
+    </div>
+  );
+}
+
+export default App;
